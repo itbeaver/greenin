006051faeb4aff4785ce1b452ec00cb7b7bc7d7c
Russkikh Artem <rusartx@gmail.com>
2015-02-23 23:51:05 +0500

    activerecord helper methods

--- lib/greenin.rb
+++ lib/greenin.rb
@@ -1,5 +1,22 @@
 require "greenin/version"
 
 module Greenin
-  # Your code goes here...
+end
+
+module ActiveRecord
+  class Relation
+  	def entity
+      self.model::Entity.new(self) if defined? self.model::Entity
+  	end
+  end
+
+  class Base
+  	def entity
+      self.class::Entity.new(self) if defined? self.class::Entity
+  	end
+
+    def self.entity
+      self::Entity if defined? self::Entity
+    end
+  end
 end
