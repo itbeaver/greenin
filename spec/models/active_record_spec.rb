2a2ee3b41930f1baeaf8c4cd5cb4881769f86229
Russkikh Artem <rusartx@gmail.com>
2015-02-24 23:15:23 +0500

    spec fails

--- spec/models/active_record_spec.rb
+++ spec/models/active_record_spec.rb
@@ -1,7 +1,15 @@
 describe Post do
   it 'Post.entity' do
     expect(Post.entity).to eq(Post::Entity)
-    expect(Post.first.entity).to eq(Post::Entity.new(Post.first))
-    expect(Post.all.entity).to eq(Post::Entity.new(Post.all))
+  end
+  it 'Post.first.entity' do
+    post = create(:post)
+    expect(post.entity).to be_instance_of(Post::Entity.new(post).class)
+    expect(post.entity.object).to eq(post)
+  end
+  it 'Post.all.entity' do
+    3.times { create(:post) }
+    expect(Post.all.entity).to be_instance_of(Post::Entity.new(Post.all).class)
+    expect(Post.all.entity.object).to eq(Post.all)
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
     end
   end
 
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
